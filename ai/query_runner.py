9ed986355ef9f73944de6b6446d37357b5af93e7
Raghav Shankar <66262040+krrakenn@users.noreply.github.com>
2026-02-02 17:43:18 +0530

    Update query_runner.py

--- ai/query_runner.py
+++ ai/query_runner.py
@@ -1,11 +1,12 @@
 import sqlite3
 import pandas as pd
 
-DB_PATH = r"db\chinook.db"
+DB_PATH = "db/chinook.db"
 
 def run_sql(sql):
     conn = sqlite3.connect(DB_PATH)
     df = pd.read_sql_query(sql, conn)
     conn.close()
     return df
+
 
