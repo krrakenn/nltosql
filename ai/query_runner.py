18532f4a140ba1d33cb638688802c7bebde4553a
Raghav Shankar <66262040+krrakenn@users.noreply.github.com>
2026-02-02 17:39:25 +0530

    Change DB_PATH to use a relative path
    Updated the database path to a relative path.

--- ai/query_runner.py
+++ ai/query_runner.py
@@ -1,10 +1,11 @@
 import sqlite3
 import pandas as pd
 
-DB_PATH = r"C:\Users\Imart\Desktop\DWProject\db\chinook.db"
+DB_PATH = r"db\chinook.db"
 
 def run_sql(sql):
     conn = sqlite3.connect(DB_PATH)
     df = pd.read_sql_query(sql, conn)
     conn.close()
     return df
+
